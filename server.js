59aa8aebab85146f6488a86dac7cf07f7e901af9
Mohammad Solaiman <mohammadsolaiman@Mohammads-MacBook-Pro.local>
2021-02-23 23:19:32 +0600

    test

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 });
 
 const db = mongoose.connection;
